46eab95ac38ea1ed204ccf0f32a7b7506825ba41
asunath <129926263+asunath@users.noreply.github.com>
2023-04-12 23:22:56 +0530

    Update basic calculator.py

--- basic calculator.py
+++ basic calculator.py
@@ -1,12 +1,9 @@
-num1=float(input("enter first number:"))
-op=input("enter first operator:")
-num2=float(input("enter second number:"))
-if op== "+":
-   print("num1+num2")
-elif op=="-":
-     print(num1-num2)
-elif op=="/":
-     print("num1/num2")
- else:
-     print("invalid operation")
+
      
+x=input("enter operands seperated with space:")
+y=input("enter the operations :")
+num1=int(x[0])
+num2=int(x[2])
+res=[num1+num2,num1-num2,num1/num2]
+for i in range(len(y)):
+   print(res[i],end=" ")
